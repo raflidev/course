7d4793f68366f0be1bc513240fbb1f308e530daa
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-06-03 23:49:50 +0700

    commit pertama

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app" class="bg-purple">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/logo.png" width="60" height="60" alt="" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">KELAS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">MADING</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ALUR BELAJAR</a>
+            </li>
+            <li class="nav-item">
+              <span class="nav-divider"></span>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn btn-dark" href="#">LOGIN</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <Home />
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,700;1,400&family=Montserrat:wght@300;400;600;700&display=swap");
+body {
+  color: #fff;
+}
+.active > .nav-link {
+  color: #31de31 !important;
+}
+.bg-purple {
+  background-color: #150e20 !important;
+}
+.text-purple {
+  color: #150e20 !important;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  line-height: 30px;
+}
+.text-merri {
+  font-family: "Merriweather", serif;
+}
+.navbar {
+  padding-top: 30px;
+  padding-right: 1rem;
+  padding-bottom: 30px;
+  padding-left: 1rem;
+}
+.navbar-nav .nav-divider {
+  width: 1px;
+  height: calc(100% - 8px);
+  display: inline-block;
+  margin: 4px 1.25rem;
+}
+.navbar-dark .navbar-nav .nav-divider {
+  background-color: #282646;
+}
+.navbar-expand-lg .navbar-nav .nav-link {
+  padding-right: 1.25rem;
+  padding-left: 1.25rem;
+}
+.font-weight-light {
+  font-weight: 300;
+}
+.btn-outline-purple {
+  color: #fff;
+  border-color: #fff;
+}
+.btn-outline-purple:hover {
+  color: #fff;
+  background-color: #491d8c;
+  border-color: #491d8c;
+}
+.text-gray-500 {
+  color: #a8a8a8 !important;
+}
+.text-600 {
+  font-weight: 600;
+}
+.line-height-1 {
+  line-height: 1.5;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,277 @@
+<template>
+  <main>
+    <section>
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-lg-7">
+            <h1 class="text-merri pb-3">
+              Build Your Future Career
+            </h1>
+            <h5 class="font-weight-light">
+              Pelajari koding untuk membuat website dan aplikasi
+              <br />
+              ingin menjenjang karir programmer atau membuat karya? <br />
+              kami tunggu dikelas!
+            </h5>
+            <form class="pt-4">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                />
+              </div>
+              <div class="form-group">
+                <button
+                  type="submit"
+                  class="btn btn-outline-purple form-control"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="col-lg-5"></div>
+        </div>
+      </div>
+    </section>
+    <section class="kelas-populer py-5 mt-5">
+      <div class="mt-5 container text-purple">
+        <h1 class="text-merri pb-3 text-center">Kelas Populer</h1>
+        <div class="row justify-content-center mt-5">
+          <div class="col col-12 col-lg-11">
+            <div class="row justify-content-center">
+              <div class="col-11 col-lg-4 mb-4">
+                <div class="card">
+                  <div class="card-body">
+                    <img
+                      src="https://www.buildwithangga.com/storage/assets/thumbnails/thumbnail_kelas_fullstack_web_developer_buildwithangga.png"
+                      class="card-img-top"
+                    />
+                    <h6 class="line-height-1 mb-0 mt-3 text-600">
+                      Full-Stack Web Developer
+                    </h6>
+                    <span class="text-gray-500">Rp. 360.000</span>
+                    <hr />
+                    <div class="row ">
+                      <div class="col-auto">
+                        <div class="mr-2 ">
+                          <img
+                            src="../assets/logo.png"
+                            width="50"
+                            height="50"
+                          />
+                        </div>
+                      </div>
+                      <div class="col mt-2 pl-2">
+                        <h6 class="mb-0 line-height-1 text-600">
+                          Rafli Ramadhan
+                        </h6>
+                        <p class="text-gray-500">Full-Stack Developer</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-11 col-lg-4 mb-4">
+                <div class="card">
+                  <div class="card-body">
+                    <img
+                      src="https://www.buildwithangga.com/storage/assets/thumbnails/thumbnail_kelas_fullstack_web_developer_buildwithangga.png"
+                      class="card-img-top"
+                    />
+                    <h6 class="line-height-1 mb-0 mt-3 text-600">
+                      Full-Stack Web Developer
+                    </h6>
+                    <span class="text-gray-500">Rp. 360.000</span>
+                    <hr />
+                    <div class="row ">
+                      <div class="col-auto">
+                        <div class="mr-2 ">
+                          <img
+                            src="../assets/logo.png"
+                            width="50"
+                            height="50"
+                          />
+                        </div>
+                      </div>
+                      <div class="col mt-2 pl-2">
+                        <h6 class="mb-0 line-height-1 text-600">
+                          Rafli Ramadhan
+                        </h6>
+                        <p class="text-gray-500">Full-Stack Developer</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-11 col-lg-4 mb-4">
+                <div class="card">
+                  <div class="card-body">
+                    <img
+                      src="https://www.buildwithangga.com/storage/assets/thumbnails/thumbnail_kelas_fullstack_web_developer_buildwithangga.png"
+                      class="card-img-top"
+                    />
+                    <h6 class="line-height-1 mb-0 mt-3 text-600">
+                      Full-Stack Web Developer
+                    </h6>
+                    <span class="text-gray-500">Rp. 360.000</span>
+                    <hr />
+                    <div class="row ">
+                      <div class="col-auto">
+                        <div class="mr-2 ">
+                          <img
+                            src="../assets/logo.png"
+                            width="50"
+                            height="50"
+                          />
+                        </div>
+                      </div>
+                      <div class="col mt-2 pl-2">
+                        <h6 class="mb-0 line-height-1 text-600">
+                          Rafli Ramadhan
+                        </h6>
+                        <p class="text-gray-500">Full-Stack Developer</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="ulasan-mereka py-5 border-top">
+      <div class="container text-purple">
+        <div class="mt-5 container text-purple">
+          <h1 class="text-merri pb-3 text-center">Ulasan Mereka</h1>
+          <div class="row justify-content-center mt-5">
+            <div class="col col-12 col-lg-12">
+              <div class="row justify-content-center">
+                <div class="col-11 col-lg-4 mb-4">
+                  <div class="card">
+                    <div class="card-body p-3">
+                      <h6 class="line-height-1">
+                        Mentornya keren cara menyampaikan setiap materinya juga
+                        detail dan mudah dipahami.
+                      </h6>
+                      <hr />
+                      <div class="row ">
+                        <div class="col-auto">
+                          <div class="mr-2 ">
+                            <img
+                              src="../assets/logo.png"
+                              width="50"
+                              height="50"
+                            />
+                          </div>
+                        </div>
+                        <div class="col pl-2">
+                          <h6 class="mb-0 line-height-1 text-600">
+                            Rafli Ramadhan
+                          </h6>
+                          <span class="text-gray-500">Tukang Bakso</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-11 col-lg-4 mb-4">
+                  <div class="card">
+                    <div class="card-body p-3">
+                      <h6 class="line-height-1">
+                        Kelas yang paling recommended pokoknya buat investasi
+                        ilmu di era revolusi industri 4.0.
+                      </h6>
+                      <hr />
+                      <div class="row ">
+                        <div class="col-auto">
+                          <div class="mr-2 ">
+                            <img
+                              src="../assets/logo.png"
+                              width="50"
+                              height="50"
+                            />
+                          </div>
+                        </div>
+                        <div class="col pl-2">
+                          <h6 class="mb-0 line-height-1 text-600">
+                            Rafli Ramadhan
+                          </h6>
+                          <span class="text-gray-500">Tukang Bakso</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-11 col-lg-4 mb-4">
+                  <div class="card">
+                    <div class="card-body p-3">
+                      <h6 class="line-height-1">
+                        Kelas yang sangat bermanfaat dan ilmu up-to-date sekali
+                        dan yang paling penting biayanya sangat terjangkau.
+                      </h6>
+                      <hr />
+                      <div class="row ">
+                        <div class="col-auto">
+                          <div class="mr-2 ">
+                            <img
+                              src="../assets/logo.png"
+                              width="50"
+                              height="50"
+                            />
+                          </div>
+                        </div>
+                        <div class="col pl-2">
+                          <h6 class="mb-0 line-height-1 text-600">
+                            Rafli Ramadhan
+                          </h6>
+                          <span class="text-gray-500">Tukang Bakso</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-outline-secondary">
+              REVIEW LAINNYA
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+.kelas-populer,
+.ulasan-mereka {
+  background-color: #fff;
+}
+h1 {
+  font-weight: 700;
+}
+h5 {
+  line-height: 30px;
+}
+</style>
