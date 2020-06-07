339a446d50506fd38dd2b883b59d327b16abd5fd
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-06-07 22:55:52 +0700

    menambah detail mading

--- src/views/detail/Mading.vue
+++ src/views/detail/Mading.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <h1>test</h1>
-  </div>
+  <main>
+    <section class="py-3 pb-5">
+      <div class="container mt-5 text-center">
+        <h1 class="text-merri pb-3">Aplikasi Perbankan</h1>
+        <h5>Rafli Ramadhan</h5>
+      </div>
+    </section>
+    <section class="bg-white text-purple py-3">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-8">
+            <div class="card">
+              <div class="card-body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vel officiis exercitationem culpa? Laboriosam vel corporis et dolore possimus provident aspernatur!</p>
+                <p>
+                  Link karya:
+                  <br />
+                  <a href="http://google.com">Google</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style>
-</style>
