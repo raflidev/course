039570158a6a8f2f9c0b38a9d425f4e8795fbf8a
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-06-14 22:26:11 +0700

    add mobile layout dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -154,7 +154,97 @@
       </div>
     </div>
     <!-- mobile layout -->
-    <div class="d-block fixed-bottom d-sm-none pt-2 py-2 bg-green">
+    <div class="header-mobile d-block d-sm-none">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <img src="@/assets/logo.png" width="60" class="float-left mr-3" />
+            <div class="user-name">
+              <p class="mb-0">Rafli Ramadhan</p>
+              <p class="font-weight-light">Web Developer</p>
+            </div>
+            <a href class="mt-3 font-weight-light text-decoration-none float-left text-white">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-block d-sm-none bg-white py-4 main-core-mobile">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h5 class="mb-0">Progress Belajar</h5>
+            <p class="text-muted">Berikut ini adalah daftar kelas yang anda ikuti</p>
+            <div class="item-kelas">
+              <div class="row">
+                <div class="col-12">
+                  <div style="float:left;width:300px">
+                    <p class="title">Fullstack Web Developer</p>
+                    <div class="progress progress-mobile my-2">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 50%;"
+                        aria-valuenow="100"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >50%</div>
+                    </div>
+                    <p class="text-muted">4 dari 10 materi telah selesai</p>
+                    <a href class="btn mr-2 btn-sm btn-outline-secondary">LANJUTKAN</a>
+                    <a href class="btn btn-sm btn-success disabled">CETAK SERTIFIKAT</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item-kelas">
+              <div class="row">
+                <div class="col-12">
+                  <div style="float:left;width:300px">
+                    <p class="title">Fullstack Web Developer</p>
+                    <div class="progress progress-mobile my-2">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 50%;"
+                        aria-valuenow="100"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >50%</div>
+                    </div>
+                    <p class="text-muted">4 dari 10 materi telah selesai</p>
+                    <a href class="btn mr-2 btn-sm btn-outline-secondary">LANJUTKAN</a>
+                    <a href class="btn btn-sm btn-success disabled">CETAK SERTIFIKAT</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item-kelas">
+              <div class="row">
+                <div class="col-12">
+                  <div style="float:left;width:300px">
+                    <p class="title">Fullstack Web Developer</p>
+                    <div class="progress progress-mobile my-2">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 50%;"
+                        aria-valuenow="100"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >50%</div>
+                    </div>
+                    <p class="text-muted">4 dari 10 materi telah selesai</p>
+                    <a href class="btn mr-2 btn-sm btn-outline-secondary">LANJUTKAN</a>
+                    <a href class="btn btn-sm btn-success disabled">CETAK SERTIFIKAT</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-block fixed-bottom d-sm-none pt-2 py-2 bg-purple">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-10">
@@ -162,7 +252,27 @@
               <div class="col-4">
                 <a href>
                   <div class="item-menu text-center">
-                    <p class="mb-0">
+                    <p class="mb-0 icon">
+                      <i class="material-icons">assessment</i>
+                    </p>
+                    <p class="mb-0 mt-0">Progress</p>
+                  </div>
+                </a>
+              </div>
+              <div class="col-4">
+                <a href>
+                  <div class="item-menu text-center">
+                    <p class="mb-0 icon">
+                      <i class="material-icons">assessment</i>
+                    </p>
+                    <p class="mb-0 mt-0">Progress</p>
+                  </div>
+                </a>
+              </div>
+              <div class="col-4">
+                <a href>
+                  <div class="item-menu text-center">
+                    <p class="mb-0 icon">
                       <i class="material-icons">assessment</i>
                     </p>
                     <p class="mb-0 mt-0">Progress</p>
@@ -181,8 +291,33 @@
 export default {};
 </script>
 <style>
+.main-core-mobile {
+  padding-bottom: 80px !important;
+}
+.progress-mobile {
+  width: 300px !important;
+}
+.header-mobile {
+  color: #fff;
+  padding: 15px;
+}
+.user-name {
+  margin: 0;
+  float: left;
+  width: 160px;
+}
+.user-name .font-weight-light {
+  font-size: 14px;
+}
 .item-menu {
   font-size: 12px;
+}
+.item-menu p {
+  margin-top: 0 !important;
+}
+.item-menu p.icon {
+  padding: 0;
+  height: 24px;
 }
 div.progress {
   width: 350px;
