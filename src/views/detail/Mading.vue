04335ca19c7dd93b0471d579914cf1814117ca5e
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-06-20 22:34:20 +0700

    remake detail mading

--- src/views/detail/Mading.vue
+++ src/views/detail/Mading.vue
@@ -12,43 +12,28 @@
           <div class="col-lg-11">
             <div class="row justify-content-center">
               <div class="col-lg-8">
-                <div class="card">
+                <div class="card mb-4">
                   <div class="card-body">
-                    <div class="row">
-                      <div class="col-lg-1">
-                        <img src="@/assets/logo.png" width="50" />
-                      </div>
-                      <div class="col-lg">
-                        <a class="text-decoration-none" href="/user/rafliram">@rafliram</a>
-                        <br />
-                        <span class="text-muted">18 menit lalu</span>
-                      </div>
-                    </div>
-                    <hr />
+                    <h4>Aplikasi Perbankan</h4>
+                    <p class="text-muted">18 menit lalu</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vel officiis exercitationem culpa? Laboriosam vel corporis et dolore possimus provident aspernatur!</p>
                     <p>
                       <span class="font-weight-bold">Link karya:</span>
                       <br />
                       <a href="http://google.com">http://google.com</a>
                     </p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="col-lg-12">
-                      <img src="@/assets/logo.png" class="float-left mr-3" width="50" alt srcset />
-                      <h6 class="mb-0 pt-1">Rafli Ramadhan</h6>
-                      <span class="text-muted">Web Developer</span>
-                    </div>
                     <hr />
-                    <div class="mt-4">
-                      <p
-                        class="text-justify"
-                      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut cupiditate deleniti quia accusamus atque maiores, quasi eos ratione sit autem quod eius. Consectetur perferendis rerum sapiente fuga molestias vel facilis!</p>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-12">
+                          <img src="@/assets/logo.png" class="float-left mr-4" width="50" />
+
+                          <p class="float-left mb-0" href="/user/rafliram">Rafli Ramadhan</p>
+                          <br />
+                          <router-link to="/user/rafliram" class="btn-link">Lihat profil</router-link>
+                        </div>
+                      </div>
                     </div>
-                    <a href="http://" class="btn btn-primary form-control">LIHAT PROFIL</a>
                   </div>
                 </div>
               </div>
